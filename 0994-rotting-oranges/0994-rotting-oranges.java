4106acbed05590d775328cb2a3051df0e7c5b8ca
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-10-25 13:48:45 +0530

    Time: 3 ms (79.83%), Space: 43.1 MB (34.43%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<Tomato> bfs = new ArrayDeque<Tomato>();
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] visited = grid;
+        int minTime = 0;
+        //adding rotten tomatoes to queue
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == 2){
+                    bfs.add(new Tomato(i,j,0));
+                }
+            }
+        }
+        while(!bfs.isEmpty()){
+        Tomato temp = bfs.poll();
+        //check 4-directionally for temp
+        //right
+        if(temp.j < n-1 && grid[temp.i][temp.j+1] == 1 && visited[temp.i][temp.j+1] != 2){
+                bfs.add(new Tomato(temp.i,temp.j+1,temp.time+1));
+                visited[temp.i][temp.j+1] = 2;
+            }
+        //left
+        if(temp.j > 0 && grid[temp.i][temp.j-1] == 1 && visited[temp.i][temp.j-1] != 2){
+                bfs.add(new Tomato(temp.i,temp.j-1,temp.time+1));
+                visited[temp.i][temp.j-1] = 2;
+            }
+        //down
+        if(temp.i < m-1 && grid[temp.i+1][temp.j] == 1 && visited[temp.i+1][temp.j] != 2){
+                bfs.add(new Tomato(temp.i+1,temp.j,temp.time+1));
+                visited[temp.i+1][temp.j] = 2;
+            }
+        //up
+        if(temp.i > 0 && grid[temp.i-1][temp.j] == 1 && visited[temp.i-1][temp.j] != 2){
+                bfs.add(new Tomato(temp.i-1,temp.j,temp.time+1));
+                visited[temp.i-1][temp.j] = 2;
+            }
+         minTime = temp.time;   
+        }
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(visited[i][j] == 1){
+                   return -1;
+                }
+            }
+        }
+        return minTime;
+        
+    }
+}
+
+
+
+
+class Tomato {
+    int i;
+    int j;
+    int time;
+    public Tomato(int i,int j,int time){
+        this.i = i;
+        this.j = j;
+        this.time = time;
+    }
+}
