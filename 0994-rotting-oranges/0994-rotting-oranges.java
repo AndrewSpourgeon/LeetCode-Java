c70154d63e3be80f2c2a8eed099d81198e9ed144
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-10-25 13:53:34 +0530

    Time: 3 ms (79.83%), Space: 43.4 MB (23.02%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -3,7 +3,6 @@ class Solution {
         Queue<Tomato> bfs = new ArrayDeque<Tomato>();
         int m = grid.length;
         int n = grid[0].length;
-        int[][] visited = grid;
         int minTime = 0;
         //adding rotten tomatoes to queue
         for(int i = 0 ; i < m ; i++){
@@ -17,30 +16,30 @@ class Solution {
         Tomato temp = bfs.poll();
         //check 4-directionally for temp
         //right
-        if(temp.j < n-1 && grid[temp.i][temp.j+1] == 1 && visited[temp.i][temp.j+1] != 2){
+        if(temp.j < n-1 && grid[temp.i][temp.j+1] == 1){
                 bfs.add(new Tomato(temp.i,temp.j+1,temp.time+1));
-                visited[temp.i][temp.j+1] = 2;
+                grid[temp.i][temp.j+1] = 2;
             }
         //left
-        if(temp.j > 0 && grid[temp.i][temp.j-1] == 1 && visited[temp.i][temp.j-1] != 2){
+        if(temp.j > 0 && grid[temp.i][temp.j-1] == 1 ){
                 bfs.add(new Tomato(temp.i,temp.j-1,temp.time+1));
-                visited[temp.i][temp.j-1] = 2;
+                grid[temp.i][temp.j-1] = 2;
             }
         //down
-        if(temp.i < m-1 && grid[temp.i+1][temp.j] == 1 && visited[temp.i+1][temp.j] != 2){
+        if(temp.i < m-1 && grid[temp.i+1][temp.j] == 1 ){
                 bfs.add(new Tomato(temp.i+1,temp.j,temp.time+1));
-                visited[temp.i+1][temp.j] = 2;
+                grid[temp.i+1][temp.j] = 2;
             }
         //up
-        if(temp.i > 0 && grid[temp.i-1][temp.j] == 1 && visited[temp.i-1][temp.j] != 2){
+        if(temp.i > 0 && grid[temp.i-1][temp.j] == 1 ){
                 bfs.add(new Tomato(temp.i-1,temp.j,temp.time+1));
-                visited[temp.i-1][temp.j] = 2;
+                grid[temp.i-1][temp.j] = 2;
             }
          minTime = temp.time;   
         }
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(visited[i][j] == 1){
+                if(grid[i][j] == 1){
                    return -1;
                 }
             }
