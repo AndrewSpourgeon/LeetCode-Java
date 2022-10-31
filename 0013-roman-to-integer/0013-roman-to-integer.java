bb53db23493cb84d4932c132aaafc0138230f74e
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-10-31 17:56:02 +0530

    Time: 7 ms (77.19%), Space: 45.5 MB (57.48%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,49 +1,47 @@
 class Solution {
-     Map<Character,Integer> romanValues=new HashMap<Character,Integer>(){{
-           put('I',1);
-           put('V',5);
-           put('X',10);
-           put('L',50);
-           put('C',100);
-           put('D',500);
-           put('M',1000);
-           
-       }};
     public int romanToInt(String s) {
-       int result=0;
-       char[] romanChars=s.toCharArray();
-       int size=romanChars.length;
-       for(int i=0;i<romanChars.length;i++){
-            if( (i<size-1 && romanChars[i]=='I' && romanChars[i+1]=='I' )||(i==size-1 && romanChars[i]=='I')){
-               result+=1;
-           }
-          else if(i<size-1 && getVal(romanChars[i])<getVal(romanChars[i+1])){
-               result+=getVal(romanChars[i+1])-getVal(romanChars[i]);
-               i=i+1;
-           }
-           else if(i<size-2 && getVal(romanChars[i])>getVal(romanChars[i+1]) && getVal(romanChars[i+1])<getVal(romanChars[i+2])){
-               result+=getVal(romanChars[i]);
-               result+=getVal(romanChars[i+2])-getVal(romanChars[i+1]);
-                i=i+2;
-                
-                
-           }
-           else if(i<size-1 && getVal(romanChars[i]) > getVal(romanChars[i+1])) {         
-               result+=getVal(romanChars[i])+getVal(romanChars[i+1]);
-               i++;
-           }
-          else if(i<size-1&&getVal(romanChars[i])==getVal(romanChars[i+1])){
-              result+=getVal(romanChars[i])+getVal(romanChars[i+1]);
-              i++;
-          }
-           else{
-               result+=getVal(romanChars[i]);
-           }
-       }
-       return result; 
+        int ans = 0;
+        int len = s.length();
+        int i = 0;
+        while( i < len ){
+            int cur = getVal(s.charAt(i));
+            if(i < len - 1 && cur < getVal(s.charAt(i+1))){
+                ans = ans + (getVal(s.charAt(i+1)) - cur);
+                i = i + 2;
+            }
+            else{
+                ans = ans + cur;
+                i = i + 1;
+            }
+        }
+        return ans;
     }
     
-   public int getVal(char c){
-       return romanValues.get(c);
+    private int getVal(char ch){
+        int ans = 0;
+        switch(ch){
+            case 'I':
+                ans = 1;
+                break;
+            case 'V':
+                ans = 5;
+                break;
+            case 'X':
+                ans = 10;
+                break;
+            case 'L':
+                ans = 50;
+                break;
+            case 'C':
+                ans = 100;
+                break;
+            case 'D':
+                ans = 500;
+                break;
+            case 'M':
+                ans = 1000;
+                break;
+        }
+        return ans;
     }
 }
