2682e6e83fa2d442f2164d05a7af6ca573961fd6
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-11-09 18:14:51 +0530

    Time: 20 ms (35.10%), Space: 42.5 MB (39.56%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String countAndSay(int n) {
+        if(n == 1){
+            return "1";
+        }
+        String temp = countAndSay(n-1);
+        String ans = say(temp);
+        return ans;
+    }
+    
+    private String say(String s){
+        if(s.length() == 1) return "1" + s;
+        int count = 1;
+        String temp = "";
+        int i = 0;
+        while( i < s.length()){
+            while( i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)){
+                count++;
+                i++;
+            }
+            temp = temp + (char)(count + '0') + s.charAt(i);
+            count = 1;
+            i++;
+            if(i == s.length()-1){
+                temp = temp + (char)(count + '0') + s.charAt(i);
+                break;
+            }
+        }
+        return temp;
+    }
+}
