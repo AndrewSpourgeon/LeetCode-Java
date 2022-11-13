1586fe567e2e5b945f49027bd5305205f4d852f8
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-11-13 20:38:03 +0530

    Time: 1 ms (48.19%), Space: 40.1 MB (99.53%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,18 +14,39 @@
  * }
  */
 class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> inOrderList = new ArrayList<>();
+//         inOrderRecursion(root ,inOrderList);
+//         return inOrderList;
+//     }
+    
+//     private void inOrderRecursion(TreeNode root , List<Integer> inorderList){
+//         if(root == null){
+//             return;
+//         }
+//         inOrderRecursion(root.left,inorderList);
+//         inorderList.add(root.val);
+//         inOrderRecursion(root.right,inorderList);
+//     }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> inOrderList = new ArrayList<>();
-        inOrderRecursion(root ,inOrderList);
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while(true){
+            if(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            else{
+                if(stack.empty()){
+                    break;
+                }
+                TreeNode temp = stack.pop();
+                inOrderList.add(temp.val);
+                root = temp.right;
+            }
+        }
         return inOrderList;
     }
     
-    private void inOrderRecursion(TreeNode root , List<Integer> inorderList){
-        if(root == null){
-            return;
-        }
-        inOrderRecursion(root.left,inorderList);
-        inorderList.add(root.val);
-        inOrderRecursion(root.right,inorderList);
-    }
+    
 }
