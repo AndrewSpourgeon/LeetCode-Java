ebc8626c54e37a19ff3b03e53e3746607fd7e4ac
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-11-13 21:27:08 +0530

    Time: 1 ms (49.61%), Space: 42.2 MB (56.21%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,20 +14,41 @@
  * }
  */
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-
-        List<Integer> inOrderList = new ArrayList<>();
-        inOrderRecursion(root ,inOrderList);
-        return inOrderList;
-    }
     
-    private void inOrderRecursion(TreeNode root , List<Integer> inorderList){
-        if(root == null){
-            return;
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> inOrderList = new ArrayList<>();
+//         inOrderRecursion(root ,inOrderList);
+//         return inOrderList;
+//     }
+    
+//     private void inOrderRecursion(TreeNode root , List<Integer> inorderList){
+//         if(root == null){
+//             return;
+//         }
+//         inorderList.add(root.val);
+//         inOrderRecursion(root.left,inorderList);
+//         inOrderRecursion(root.right,inorderList);
+//     }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preOrderList = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while(true){
+            if(root != null){
+                preOrderList.add(root.val);
+                stack.push(root);
+                root = root.left;
+            }
+            else{
+                if(stack.empty()){
+                    break;
+                }
+                else{
+                    TreeNode temp = stack.pop();
+                    root = temp.right;
+                }
+            }
         }
-        inorderList.add(root.val);
-        inOrderRecursion(root.left,inorderList);
-        inOrderRecursion(root.right,inorderList);
+        return preOrderList;
     }
     
 }
