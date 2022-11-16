fd1e7fd35f12d1523f81219cf461056da7de6fd3
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-11-16 10:41:24 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (38.32%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,7 +14,8 @@
  * }
  */
 class Solution {
-    
+//     //-> solution 1
+//     // recursion
 //     public List<Integer> preorderTraversal(TreeNode root) {
 //         List<Integer> inOrderList = new ArrayList<>();
 //         inOrderRecursion(root ,inOrderList);
@@ -29,26 +30,62 @@ class Solution {
 //         inOrderRecursion(root.left,inorderList);
 //         inOrderRecursion(root.right,inorderList);
 //     }
+    
+//     //-> solution 2
+//     // Iterative
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         List<Integer> preOrderList = new ArrayList<>();
+//         Stack<TreeNode> stack = new Stack<TreeNode>();
+//         while(true){
+//             if(root != null){
+//                 preOrderList.add(root.val);
+//                 stack.push(root);
+//                 root = root.left;
+//             }
+//             else{
+//                 if(stack.empty()){
+//                     break;
+//                 }
+//                 else{
+//                     TreeNode temp = stack.pop();
+//                     root = temp.right;
+//                 }
+//             }
+//         }
+//         return preOrderList;
+//     }
+    
+    //solution -> 3
+    //Morris In-Order Algorithm
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> preOrderList = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        while(true){
-            if(root != null){
-                preOrderList.add(root.val);
-                stack.push(root);
-                root = root.left;
+        TreeNode cur = root;
+        while(cur != null){
+            //if cur left is null then add to list and move to right
+            if(cur.left == null){
+                preOrderList.add(cur.val);
+                cur = cur.right;
             }
+            //
             else{
-                if(stack.empty()){
-                    break;
+                TreeNode temp = cur.left;
+                while(temp.right != null && temp.right != cur){
+                    temp = temp.right;
+                }
+                //now we reached end point so create link
+                if(temp.right == null){
+                    temp.right = cur;
+                    preOrderList.add(cur.val);
+                    cur = cur.left;
                 }
                 else{
-                    TreeNode temp = stack.pop();
-                    root = temp.right;
+                    temp.right = null;
+                    cur = cur.right;
                 }
+                
             }
         }
-        return preOrderList;
+    return preOrderList;
     }
     
 }
