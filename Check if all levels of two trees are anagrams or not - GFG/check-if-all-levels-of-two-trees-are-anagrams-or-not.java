9a3cc5055363a812edd12219846194f5a0c0a8de
Andrew Spourgeon <77846804+AndrewSpourgeon@users.noreply.github.com>
2022-11-19 10:05:49 +0530

    Added solution - LeetHub

---
+++ Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.java
@@ -0,0 +1,176 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+    
+    public static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        
+        // Creating array of Strings from input 
+        // String after spliting by space
+        String ip[] = str.split(" ");
+        
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        
+        // Push the root to the queue
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(queue.size()>0 && i < ip.length)
+        {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N"))
+            {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+    public static Node inputTree(BufferedReader br) throws IOException
+    {
+        return buildTree(br.readLine().trim());
+    }
+    
+    public static void inorder(Node root)
+    {
+        if (root == null)
+           return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            Node node1 = Node.inputTree(br);
+            
+            
+            Node node2 = Node.inputTree(br);
+            
+            Solution obj = new Solution();
+            boolean res = obj.areAnagrams(node1, node2);
+            
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*
+
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+    
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public static boolean areAnagrams(Node node1, Node node2) {
+        // code here
+        Queue<Node> tree1 = new LinkedList<Node>();
+        Queue<Node> tree2 = new LinkedList<Node>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        tree1.add(node1);
+        tree2.add(node2);
+        if(tree1.peek().data != tree2.peek().data) return false;
+        while(!tree1.isEmpty()){
+            int size1 = tree1.size();
+            for(int i = 0 ; i < size1 ; i++){
+                Node temp = tree1.poll();
+                if(temp.left != null) tree1.add(temp.left);
+                if(temp.right != null) tree1.add(temp.right);
+                list.add(temp.data);
+            }
+            int size2 = tree2.size();
+            for(int i = 0 ; i < size2 ; i++){
+                Node temp = tree2.poll();
+                if(temp.left != null) tree2.add(temp.left);
+                if(temp.right != null) tree2.add(temp.right);
+                if(!list.contains(temp.data)){
+                    return false;
+                }
+                else if(list.contains(temp.data)){
+                    list.remove(new Integer(temp.data));
+                }
+                
+            }
+            if(list.size() != 0){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
+        
